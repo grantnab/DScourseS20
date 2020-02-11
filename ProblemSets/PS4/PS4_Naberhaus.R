842f295fec08e8ac4a6b06ce2abf8b5fe43af602
Grant Naberhaus <grantnaberhaus@gmail.com>
2020-02-11 13:47:48 -0600

    edit PS4

--- ProblemSets/PS4/PS4_Naberhaus.R
+++ ProblemSets/PS4/PS4_Naberhaus.R
@@ -1,10 +1,10 @@
 #I was unable to get this to work
 
 library(jsonlite)
-system("linux shell command")
-wget -o stats.json "http://api.fantasy.
-nfl.com/v1/players/stats?statType=seasonStats&season=2010&week=1&format=json"
+system("wget -o stats.json 'http://api.fantasy.
+       nfl.com/v1/players/stats?statType=seasonStats&season=2010&week=1&format=json'')
 #I was unable to download using wget, but I downloaded the JSON from the URL
+
 cat stats.json
 mydf <- fromJSON('stats.json')
 class(mydf$players)
